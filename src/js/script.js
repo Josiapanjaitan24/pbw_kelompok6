c6966823dcd6b2ff8c2895c383efcaaffb97840b
unknown <josia.panjaitan24@gmail.com>
2025-11-23 00:09:18 +0700

    Menambahkan fitur detail produk: update home.html, script.js, style.css dan tambah detail.html & detail.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,73 @@
-// Simpan produk ke localStorage untuk keranjang
-function tambahKeKeranjang(nama, harga, gambar) {
-  let keranjang = JSON.parse(localStorage.getItem('keranjang')) || [];
-  keranjang.push({ nama, harga, gambar });
-  localStorage.setItem('keranjang', JSON.stringify(keranjang));
-  alert(`${nama} telah dimasukkan ke keranjang 🛒`);
-}
-
-// Simpan produk sementara untuk halaman pesanan
-function pesanSekarang(nama, harga, gambar) {
-  localStorage.setItem('produk_dipesan', JSON.stringify({ nama, harga, gambar }));
-  window.location.href = 'order.html';
-}
-
+// Daftar semua produk
+const produkList = [
+  {
+    id: 1,
+    nama: "Kemeja Navy",
+    harga: 180000,
+    gambar: "../src/img/kemeja navy.jpg",
+    deskripsi: "Kemeja navy berkualitas dengan bahan premium dan nyaman dipakai."
+  },
+  {
+    id: 2,
+    nama: "Kemeja flanel",
+    harga: 150000,
+    gambar: "../src/img/flanel.jpg",
+    deskripsi: "flanel."
+  },
+  {
+    id: 3,
+    nama: "Batik",
+    harga: 175000,
+    gambar: "../src/img/batik.jpg",
+    deskripsi: "batik."
+  },
+  {
+    id: 4,
+    nama: "Blazer",
+    harga: 95000,
+    gambar: "../src/img/blazer.jpg",
+    deskripsi: "blazer"
+  },
+  {
+    id: 5,
+    nama: "Kemeja Hitam",
+    harga: 150000,
+    gambar: "../src/img/kemeja hitam.jpg",
+    deskripsi: "Kemeja hitam."
+  },
+  {
+    id: 6,
+    nama: "Kaos Hitam",
+    harga: 100000,
+    gambar: "../src/img/kaos hitam.jpg",
+    deskripsi: "Kaos Hitam"
+  },
+  {
+    id: 7,
+    nama: "Kaos Putih",
+    harga: 100000,
+    gambar: "../src/img/kaos putih.jpg",
+    deskripsi: "Kaos Putih."
+  },
+  {
+    id: 8,
+    nama: "Polo Hitam",
+    harga: 150000,
+    gambar: "../src/img/polo hitam.jpg",
+    deskripsi: "Polo Hitam"
+  },
+  {
+    id: 9,
+    nama: "Polo Merah",
+    harga: 150000,
+    gambar: "../src/img/polo merah.jpg",
+    deskripsi: "polo merah"
+  },
+  {
+    id: 10,
+    nama: "Polo Putih",
+    harga: 150000,
+    gambar: "../src/img/polo putih.jpg",
+    deskripsi: "Polo Putih"
+  }
+];
